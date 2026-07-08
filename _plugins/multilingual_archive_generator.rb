f86ca8c81bc8e15ee137d41edefd4deb59d7c032
rforrupesh <rupeshkumar4775@gmail.com>
2026-07-08 11:38:02 +0530

    Update multilingual_archive_generator.rb

--- _plugins/multilingual_archive_generator.rb
+++ _plugins/multilingual_archive_generator.rb
@@ -1,117 +1,100 @@
-# frozen_string_literal: true
-
 module Jekyll
-
-  class MultilingualArchivePage < Page
-
-    def initialize(site, base, dir, archive_type, archive_name, lang, posts)
-
-      @site = site
-      @base = base
-      @dir  = dir
-      @name = "index.html"
-
-      process(@name)
-      read_yaml(File.join(base, "_layouts"), "archive.html")
-
-      self.data["title"] = archive_name
-      self.data["lang"] = lang
-      self.data["posts"] = posts
-      self.data["archive_type"] = archive_type
-      self.data["archive_name"] = archive_name
-
-      self.data["permalink"] = "#{dir}/"
-
-      self.data["description"] =
-        "#{archive_type.capitalize} archive for #{archive_name}"
-
-    end
-
-  end
-
-
-
   class MultilingualArchiveGenerator < Generator
-
     safe true
     priority :low
 
+    LANGS = %w[es en fr].freeze
+
     def generate(site)
+      LANGS.each do |lang|
+        lang_posts = site.posts.docs.select { |p| p.data['lang'] == lang }
+        next if lang_posts.empty?
 
-      archives = {}
+        # URL prefix per language
+        prefix = lang == 'es' ? '' : "/#{lang}"
 
-      site.posts.docs.each do |post|
+        generate_categories(site, lang, lang_posts, prefix)
+        generate_tags(site, lang, lang_posts, prefix)
+      end
+    end
 
-        lang = post.data["lang"] || site.config["default_lang"] || "es"
+    private
 
-        archives[lang] ||= {
-          "categories" => {},
-          "tags" => {}
-        }
-
-        Array(post.data["categories"]).each do |cat|
-
-          archives[lang]["categories"][cat] ||= []
-          archives[lang]["categories"][cat] << post
-
+    def generate_categories(site, lang, posts, prefix)
+      # Collect all categories
+      cat_map = Hash.new { |h, k| h[k] = [] }
+      posts.each do |post|
+        Array(post.data['categories']).each do |cat|
+          cat_map[cat] << post
         end
-
-        Array(post.data["tags"]).each do |tag|
-
-          archives[lang]["tags"][tag] ||= []
-          archives[lang]["tags"][tag] << post
-
-        end
-
       end
 
-          #
-      # Generate Archives
-      #
-      archives.each do |lang, groups|
+      cat_map.each do |category, cat_posts|
+        slug = slugify(category)
+        url  = "#{prefix}/category/#{slug}/"
 
-        prefix = lang == "es" ? "" : "/#{lang}"
+        page = ArchivePage.new(site, url, {
+          'layout'     => 'archive',
+          'title'      => category,
+          'type'       => 'category',
+          'lang'       => lang,
+          'posts'      => cat_posts.sort_by { |p| p.date }.reverse,
+          'permalink'  => url,
+          'description'=> "Posts about #{category}"
+        })
 
-        #
-        # Categories
-        #
-        groups["categories"].each do |name, posts|
-
-          slug = Jekyll::Utils.slugify(name)
-
-          page = MultilingualArchivePage.new(
-            site,
-            site.source,
-            "#{prefix}/category/#{slug}",
-            "category",
-            name,
-            lang,
-            posts
-          )
-
-          site.pages << page
-
-        end
-
-        #
-        # Tags
-        #
-        groups["tags"].each do |name, posts|
-
-          slug = Jekyll::Utils.slugify(name)
-
-          page = MultilingualArchivePage.new(
-            site,
-            site.source,
-            "#{prefix}/tag/#{slug}",
-            "tag",
-            name,
-            lang,
-            posts
-          )
-
-          site.pages << page
-
-        end
-
+        site.pages << page
       end
+    end
+
+    def generate_tags(site, lang, posts, prefix)
+      tag_map = Hash.new { |h, k| h[k] = [] }
+      posts.each do |post|
+        Array(post.data['tags']).each do |tag|
+          tag_map[tag] << post
+        end
+      end
+
+      tag_map.each do |tag, tag_posts|
+        slug = slugify(tag)
+        url  = "#{prefix}/tag/#{slug}/"
+
+        page = ArchivePage.new(site, url, {
+          'layout'     => 'archive',
+          'title'      => tag,
+          'type'       => 'tag',
+          'lang'       => lang,
+          'posts'      => tag_posts.sort_by { |p| p.date }.reverse,
+          'permalink'  => url,
+          'description'=> "Posts tagged #{tag}"
+        })
+
+        site.pages << page
+      end
+    end
+
+    def slugify(str)
+      str.downcase.strip.gsub(/[^a-z0-9\-]/, '-').gsub(/-+/, '-').gsub(/^-|-$/, '')
+    end
+  end
+
+  class ArchivePage < Page
+    def initialize(site, url, data)
+      @site = site
+      @base = site.source
+      @dir  = url
+      @name = 'index.html'
+
+      process(@name)
+
+      @data = Jekyll::Utils.deep_merge_hashes(
+        site.frontmatter_defaults.all(url, :pages),
+        data
+      )
+    end
+
+    def url
+      @data['permalink']
+    end
+  end
+end
